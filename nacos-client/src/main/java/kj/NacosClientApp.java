d53ff8542b1ec67d41b88d1950504ddb8fe1babe
chenzhubing <Czb222333441>
2023-02-09 17:49:20 +0800

    2

--- nacos-client/src/main/java/kj/NacosClientApp.java
+++ nacos-client/src/main/java/kj/NacosClientApp.java
@@ -5,13 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class NacosFeginClientApp {
+public class NacosClientApp {
     public static void main(String[] args) {
-        SpringApplication.run(NacosFeginClientApp.class,args);
+        SpringApplication.run(NacosClientApp.class,args);
     }
 
     @Bean
--- nacos-server/src/main/java/com/kj/NacosServerApp.java
+++ nacos-server/src/main/java/com/kj/NacosServerApp.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class NacosFeginServerApp {
+public class NacosServerApp {
     public static void main(String[] args) {
-        SpringApplication.run(NacosFeginServerApp.class,args);
+        SpringApplication.run(NacosServerApp.class,args);
     }
 
     @GetMapping("message")
